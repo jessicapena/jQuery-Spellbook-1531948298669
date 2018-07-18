44bacbb4307921fa47ca664c20ccf243869428d2
jessicapena <41160831+jessicapena@users.noreply.github.com>
2018-07-18 17:11:44 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,11 @@
+$("#first").click(function() {
+    $("#firstimg").slideToggle();
+});
+
+$("#second").click(function() {
+    $("#secondimg").fadeToggle();
+});
+
+$("#third").click(function() {
+    $("#thirdimg").toggle();
+});
